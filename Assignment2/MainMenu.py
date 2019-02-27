a23fcaee148bfeca1200b29a7f4710b5fbd9d6b3
ColeGotelli <32561460+ColeGotelli@users.noreply.github.com>
2019-02-26 21:52:01 -0800

    Update files to working version

--- Assignment2/MainMenu.py
+++ Assignment2/MainMenu.py
@@ -1,10 +1,10 @@
 import sqlite3
-from Student import Student
+from Students import Students
 import StudentFunctions
 
 running = True
 
-conn = sqlite3.connect('Students.db.sqlite')
+conn = sqlite3.connect('Students.db')
 c = conn.cursor()
 
 print ("Welcome to the Portal")
@@ -28,9 +28,47 @@ while(running):
         print("To choose a student by GPA press 1")
         print("To choose a student by Major press 2")
         print("To choose a student by Advisor press 3")
-        sub_menu = raw_input("Enter Here: ")
+        sub_input = 0
+        while (sub_input == 0):
+            sub_menu = raw_input("Enter Here: ")
+
+            if (sub_menu == '1'):
+                user_gpa = raw_input("What is the GPA you are looking for: ")
+                print (StudentFunctions.selectGPA(c, user_gpa))
+                sub_input = 1
+            elif (sub_menu == '2'):
+                user_major = raw_input("What Major are looking for: ")
+                print (StudentFunctions.selectMajor(c, user_major))
+                sub_input = 1
+            elif (sub_menu == '3'):
+                user_advisor = raw_input("Who is the Advisor you are looking for: ")
+                StudentFunctions.selectAdvisor(c, user_advisor)
+                sub_input = 1
+            else:
+                print("That was an invalid input, try again")
+                sub_input = 0
+    elif (option_menu == '3'):
+        StudentFunctions.createStudents(c)
+
+    elif (option_menu == '4'):
+        print("Please Enter the ID of the Student you want to delete")
+        user_delete = raw_input("Enter Here: ")
+        StudentFunctions.deleteRecord(c, user_delete)
+
+    elif (option_menu == '5'):
+        print("Please enter the ID of the Student you want to update")
+        user_ID = raw_input("Enter Here: ")
+        new_major = raw_input("The new major is: ")
+        new_advisor = raw_input("The new advisor is: ")
+        StudentFunctions.updateStudentMajor(c, new_major, user_ID)
+        StudentFunctions.updateStudentAdvisor(c, new_advisor, user_ID)
 
     elif (option_menu == '6'):
         print("Thanks for using the Portal. Goodbye")
         running = False
 
+    else:
+        print("That was an invalid input, try again")
+        running = True
+
+    conn.commit()
--- Assignment2/StudentFunctions.py
+++ Assignment2/StudentFunctions.py
@@ -1,42 +1,49 @@
 import sqlite3
-from Student import Student
+from Students import Students
 
-class StudentFunctions:
+def selectEverything(c):
+    c.execute("select * from Student")
+    all_rows = c.fetchall()
+    print(all_rows)
 
-    def selectEverything(c):
-        c.execute("select * from Students")
-        all_rows = c.fetchall()
+def selectMajor(c, user_major):
+    c.execute("select * from Student WHERE Major = ?", (user_major,))
+    all_rows = c.fetchall()
+    print(all_rows)
 
-    def selectMajor(c, user_major):
-        c.execute("select * from Students WHERE Major = ?", (user_major,))
-        all_rows = c.fetchall()
+def selectGPA(c, user_gpa):
+    c.execute("select * from Student WHERE GPA = ?", (user_gpa,))
+    all_rows = c.fetchall()
+    print(all_rows)
 
-    def selectGPA(c, user_gpa):
-        c.execute("select * from Students WHERE GPA = ?", (user_gpa,))
-        all_rows = c.fetchall()
+def selectAdvisor(c, user_advisor):
+    c.execute("select * from Student WHERE FacultyAdvisor = ?", (user_advisor,))
+    all_rows = c.fetchall()
+    print(all_rows)
 
-    def selectAdvisor(c, user_advisor):
-        c.execute("select * from Students WHERE FacultyAdvisor = ?", (user_advisor,))
-        all_rows = c.fetchall()
+def createStudents(c):
+    fName = raw_input("Enter Student First Name: ")
+    lName = raw_input("Enter Student Last Name: ")
+    major = raw_input("Enter Major: ")
+    gpa = raw_input("Enter Student GPA: ")
+    advisor = raw_input("Enter Faculty Advisor: ")
 
-    def createStudents(c):
-        fName = raw_input("Enter Student First Name: ")
-        lName = raw_input("Enter Student Last Name: ")
-        major = raw_input("Enter Major: ")
-        gpa = raw_input("Enter Student GPA: ")
-        advisor = raw_input("Enter Faculty Advisor: ")
+    stu = Students(fName, lName, major, gpa, advisor)
 
-        stu = Student(fName, lName, major, gpa, advisor)
+    c.execute("INSERT INTO Student('FirstName', 'LastName', 'Major', 'GPA', 'FacultyAdvisor')"
+              "VALUES (?, ?, ?, ?, ?)", stu.student_info())
 
-        c.execute("INSERT INTO students('FirstName', 'LastName', 'Major', 'GPA', 'FacultyAdvisor')"
-                  "VALUES (?, ?, ?, ?, ?)", stu.student_info())
+def deleteRecord(c, user_delete):
+    c.execute("delete from Student where StudentID = " + user_delete)
+    all_rows = c.fetchall()
+    print(all_rows)
 
-    def deleteRecord(c, user_delete):
-        c.execute("delete from Students where StudentID = " + user_delete)
-        all_rows = c.fetchall()
+def updateStudentMajor(c, new_major, stuID):
+    c.execute("update Student set Major = ? where StudentID = ?", (new_major, stuID,))
+    all_rows = c.fetchall()
+    print(all_rows)
 
-    def updateStudent(c, new_major, new_advisor):
-        c.execute("update Students set Major = ?", (new_major,))
-        all_rows = c.fetchall()
-        c.execute("update Students set FacultyAdvisor = ?", (new_advisor,))
-        all_rows = c.fetchall()
+def updateStudentAdvisor(c, new_advisor, stuID):
+    c.execute("update Student set FacultyAdvisor = ? where StudentID = ?", (new_advisor, stuID,))
+    all_rows = c.fetchall()
+    print(all_rows)
